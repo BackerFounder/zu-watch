0a2dd002910ecd2a40dd47601972b0efbb41ce4e
DopplerKuo <doppler.kuo@gmail.com>
2017-02-13 03:21:57 +0800

    加上小兔兔創立一個 vue 空物件

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,4 +10,62 @@ $(window).load(function() {
 
 
 
+$(document).ready(function() {
+   // vue
+  var ZuWatch = new Vue ({
+    el: '#zu-watch',
+
+    data: {
+      preview: {
+      	prev: {
+      		a: null,
+      		b: null,
+      		c: null,
+      	},
+      	now: {
+      		a: 1,
+      		b: 1,
+      		c: 1,
+      	},
+      	next: {
+      		a: null,
+      		b: null,
+      		c: null,
+      	}
+      },
+      save: {
+      	saveA: {
+      		a: 2,
+      		b: 3,
+      		c: 3
+      	},
+      	saveB: {
+      		a: 5,
+      		b: 4,
+      		c: 1
+      	},
+      	saveC: {
+      		a: 2,
+      		b: 5,
+      		c: 2
+      	},
+      	saveD: {
+      		a: 1,
+      		b: 4,
+      		c: 3
+      	}
+      }
+    },
+
+    created: function() {
+      this.fetchData()
+    },
+
+    methods: {
+    	fetchData: function() {
+    		var self = this
+    	}
+    }
+  })
+}); // document ready end
 
