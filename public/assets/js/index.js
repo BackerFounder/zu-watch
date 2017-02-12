792247cfd3a37305226fe94592629f00eb696860
DopplerKuo <doppler.kuo@gmail.com>
2017-02-13 01:21:11 +0800

    先上兔兔版本靜態網頁不能用

---
+++ public/assets/js/index.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+	$(".elements-tab-btn").click(function() {
+		var t = $(this).attr('data-tab')
+		$(".elements-tab-btn").removeClass('active')
+		$(".elements-content-box").removeClass('active')
+		$(".elements-tab-btn[data-tab='" + t + "']").addClass('active')
+		$(".elements-content-box[data-tab='" + t + "']").addClass('active')
+	});
+})
+
+
+
+
